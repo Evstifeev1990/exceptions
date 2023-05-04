9d5b163116331cf5daedd5502b5c6a97a6e52096
Yaroslav Evstifeev <krakurs@yandex.ru>
2023-05-04 16:00:28 +0300

    add Domashka1

--- Exception/Domashka1/main1.java
+++ Exception/Domashka1/main1.java
@@ -2,8 +2,13 @@ package Exception.Domashka1;
 
 public class main1 {
     public static void main(String[] args) {
-        Integer[] arr = {1, 4, 5, 2, null, 5, 7, 5,};
-        checkArray(arr);
+        // Integer[] arr = {1, 4, 5, 2, null, 5, 7, 5,};
+        // checkArray(arr);
+        Integer[] arr1 = {1, 4, 5, 2, 5, 7, 5};
+        Integer[] arr2 = {1, 4, 5, 2, 5, 7};
+
+        checkArray1(arr1, arr2);
+        
     }
 
     public static void checkArray(Integer[] arr) {
@@ -15,4 +20,20 @@ public class main1 {
         }
             System.out.print("В массиве NULL не найден");
     }
+    
+    public static void checkArray1 (Integer[] arr1, Integer[] arr2) {
+        
+        if (arr1.length != arr2.length) {
+            throw new RuntimeException("Длины двух массивов не равны");
+        }
+        else {
+            Integer[] arr3 = new Integer[arr1.length];
+            for (int i = 0; i < arr1.length; i++) {
+                arr3[i] = arr1[i] + arr2[i];
+                System.out.print(arr3[i] + " ");
+            }
+        }
+    }
+
+
 }
